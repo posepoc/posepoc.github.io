cde597ffca820a49c620505b1574727926750c0a
ViNay Shanubhag <tech.vinaybv@gmail.com>
2020-05-29 23:14:56 +0530

    init
    init

---
+++ precache-manifest.f2a3840156a1c776a89acabb95ea42f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffc087508d069e03f399aff81b1002e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cab29486de97326cb6d2",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5098895b8e82cbfa8c61",
+    "url": "/static/js/2.b709fd8f.chunk.js"
+  },
+  {
+    "revision": "9f3ece4be77bc30b04966fe84f4cf2e7",
+    "url": "/static/js/2.b709fd8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab29486de97326cb6d2",
+    "url": "/static/js/main.10654963.chunk.js"
+  },
+  {
+    "revision": "b12bfd7025f78b30b4c1",
+    "url": "/static/js/runtime-main.2b0f3801.js"
+  }
+]);
